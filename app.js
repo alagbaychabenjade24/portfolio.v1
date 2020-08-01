f028b2cd00343e7e09a52bc34aaf67a46d9d4de5
Chaben Jade Alagbay <alagbaychabenjade24@gmail.com>
2020-08-01 18:33:43 +0800

    media queries added

---
+++ app.js
@@ -0,0 +1,18 @@
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    menu.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        menu.classList.toggle('toggle');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+        });
+    });
+};
+
+navSlide();
